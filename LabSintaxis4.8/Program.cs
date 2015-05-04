b67dbb171ca9a874ac9645d890960d3136f97569
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2015-05-04 16:06:49 -0300

    4.8 terminado

--- LabSintaxis4.8/Program.cs
+++ LabSintaxis4.8/Program.cs
@@ -10,6 +10,25 @@ namespace LabSintaxis4._8
     {
         static void Main(string[] args)
         {
+            int intentos = 0;
+            string clave, ingresado;
+            clave = "12345";
+            for (int i = 0; intentos < 4; i++)
+                {
+                    intentos += 1;
+                    Console.WriteLine("Introduzca clave");
+                    ingresado = Console.ReadLine();
+                    if (string.Equals(clave, ingresado))
+                    {
+                        Console.WriteLine("Clave correcta");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Clave incorrecta");
+                    }
+                }
+            Console.ReadKey();
         }
     }
 }
